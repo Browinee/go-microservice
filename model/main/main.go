4282f67f7677dc06fad98b321136000eaaf884c9
Browinee <nihilitypeo@gmail.com>
2023-05-14 23:02:07 +0800

    feat: add db connection

---
+++ model/main/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"go-microservice/model"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func main(){
+	dsn := "root:root1234@tcp(localhost:3306)/mxshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			 LogLevel: logger.Info,
+			 Colorful: true,
+		},
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger:newLogger,
+	})
+if err != nil {
+	panic(err)
+}
+_ = db.AutoMigrate(&model.User{})
+}
