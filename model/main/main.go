8ed13ae50b0da8034582362a84a7231ddee8f8d0
Browinee <nihilitypeo@gmail.com>
2023-05-24 19:32:04 +0800

    feat: add user.proto

--- model/main/main.go
+++ model/main/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"go-microservice/model"
 	"log"
+	"math/rand"
 	"os"
 	"time"
 
@@ -11,6 +14,23 @@ import (
 	"gorm.io/gorm/logger"
 )
 
+
+
+
+
+func generatePassword(length int) string {
+	const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	charactersLength := len(characters)
+	rand.Seed(time.Now().UnixNano())
+
+	password := make([]byte, length)
+	for i := 0; i < length; i++ {
+		randomIndex := rand.Intn(charactersLength)
+		password[i] = characters[randomIndex]
+	}
+
+	return string(password)
+}
 func main(){
 	dsn := "root:root1234@tcp(localhost:3306)/mxshop_user_srv?charset=utf8mb4&parseTime=True&loc=Local"
 	newLogger := logger.New(
